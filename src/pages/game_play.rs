f6f8a2ba785bac5c464384685f632eb1bf02ba88
mabori <mabori@ttu.ee>
2020-11-07 13:21:40 +0200

    Hudczanskij

--- src/pages/game_play.rs
+++ src/pages/game_play.rs
@@ -1,11 +1,12 @@
-use cat_engine::{WindowPage, MouseScrollDelta, KeyboardButton, ModifiersState, MouseButton, PagedWindow, Window, window_center, window_height, window_rect};
+use cat_engine::{WindowPage, MouseScrollDelta, KeyboardButton, ModifiersState, MouseButton, PagedWindow, Window, window_center, window_height, window_rect, window_width};
 use cat_engine::image::RgbaImage;
 use cat_engine::texture::{Texture, ImageBase};
 use crate::{map_background_index, character_image_index};
-use lib::colours::White;
-use cat_engine::graphics::ColourFilter;
+use lib::colours::{White, Cyan};
+use cat_engine::graphics::{ColourFilter, ObjectType, DrawType};
 use std::path::PathBuf;
-use cat_engine::shapes::RectangleWithBorder;
+use cat_engine::shapes::{RectangleWithBorder, Rectangle};
+use lib::{TextView, TextViewSettings, Button, ButtonSettings, ComplexDrawable, DrawableObject, Drawable};
 
 pub struct GamePlay{
     //character:Character,
@@ -135,9 +136,90 @@ impl WindowPage<'static> for GamePlay{
 struct Hud{
     health_bar_size: RectangleWithBorder,
     energy_bar_size: RectangleWithBorder,
-    health_bar: RectangleWithBorder,
-    energy_bar: RectangleWithBorder,
-    health_text:Text,
-    energy_text:
+    health_bar: Rectangle,
+    energy_bar: Rectangle,
+    health_text:TextView,
+    energy_text:TextView,
+}
+impl Hud{
+    fn new(&mut window:PagedWindow)->Self{
+        let health_bar_size=[40f32, 40f32,450f32,50f32];
+        let energy_bar_size=unsafe{[window_width-40f32-450f32,40f32,450f32,50f32]};
+        let health_bar_background=RectangleWithBorder::raw(health_bar_size,[1f32,1f32,1f32,0.3], 1f32,White);
+        let energy_bar_background=RectangleWithBorder::raw(energy_bar_size, [1f32,1f32,1f32,0.3],1f32,White);
+        let health_text_settings = TextViewSettings::new("Здроровье",[health_bar_size[0] + health_bar_size[3] + 5f32, health_bar_size[2], 150f32, 40f32]);
+        let health_text=TextView::new(health_text_settings,window.graphics2d());
+        let energy_text_settings = TextViewSettings::new("Энергия",[energy_bar_size[0]  - 150f32, energy_bar_size[2], 150f32, 40f32]);
+        let energy_text=TextView::new(energy_text_settings,window.graphics2d());
+        let mut health_bar = Rectangle::new(health_bar_size, [0f32,1f32,0f32,1f32]); //
+        let mut energy_bar = Rectangle::new(energy_bar_size, Cyan);
+        Self{
+            health_bar_size: health_bar_background,
+            energy_bar_size: energy_bar_background,
+            health_bar,
+            energy_bar,
+            health_text,
+            energy_text
+        }
+    }
+    fn update(&self, health:f32, energy:f32)->Self{
+    }
+}
+struct PauseMenuDialog{
+    button_yes:Button,
+    button_no:Button,
+    header:TextView,
+    dialog_box:usize,
+}
+impl PauseMenuDialog {
+    fn new(window: &mut PagedWindow) -> Self {
+        // making a box for dialog to fit in
+        let window_size = unsafe { [window_width, window_height] };
+        let rect_size = [window_size[0] / 2f32 - 200f32, window_size[1] / 2f32 - 100f32, 400f32, 200f32];
+        let dialog_box_rect = Rectangle::new(rect_size, [1.0, 0.545, 0.349, 0.75]); // Uses Bleak_orange with lowered alpha
+        let dialog_box_rect_index = window.graphics2d().add_simple_object(&dialog_box_rect).unwrap();
+
+        // making confirmation text
+        let confirmation_text_settings = TextViewSettings::new("Выйти в меню?", [window_size[0] / 2f32 - 200f32 + 5f32, window_size[1] / 2f32 - 100f32 + 5f32, 195f32, 20f32]);
+        let confirmation_text = TextView::new(confirmation_text_settings, window.graphics2d());
+
+        // making confirmation buttons
+        let confirmation_button_size = [window_size[0] / 16f32, window_size[1] / 10f32];
+        let confirmation_button_yes_placement = [window_size[0] / 2f32 - 200f32 + 20f32, window_size[1] / 2f32 - 20f32, confirmation_button_size[0], confirmation_button_size[1]];
+        let confirmation_button_yes_settings = ButtonSettings::new("Да", confirmation_button_yes_placement);
+        let confirmation_button_yes = Button::new(confirmation_button_yes_settings, window.graphics2d());
+        let confirmation_button_no_placement = [window_size[0] / 2f32 + 175f32 - confirmation_button_size[1], window_size[1] / 2f32 - 20f32, confirmation_button_size[0], confirmation_button_size[1]];
+        let confirmation_button_no_settings = ButtonSettings::new("Нет", confirmation_button_no_placement);
+        let confirmation_button_no = Button::new(confirmation_button_no_settings, window.graphics2d());
+        Self {
+            button_yes: confirmation_button_yes,
+            button_no: confirmation_button_no,
+            header: confirmation_text,
+            dialog_box: dialog_box_rect_index
+        }
+    }
+}
+impl ComplexDrawable for PauseMenuDialog{
+    fn drawables(&self)->Vec<DrawableObject>{
+        let mut drawables = Vec::with_capacity(4);
+        let drawable = DrawableObject{
+            index: self.dialog_box,
+            object_type: ObjectType::Simple,
+            draw_type: DrawType::Common,
+        };
+        drawables.push(drawable);
+        drawables.push(self.header.drawable());
+        drawables.append(&mut self.button_yes.drawables());
+        drawables.append(&mut self.button_no.drawables());
+        drawables
+    }
+}
+impl ComplexClickable for ExitConfirmationDialogue{
+    fn clickables(&self)->Vec<ClickableObject>{
+        let mut clickable = Vec::with_capacity(2);
+        clickable.append(&mut self.button_yes.clickables());
+        clickable.append(&mut self.button_no.clickables());
+        clickable
+    }
 
 }
